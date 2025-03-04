20d080da3e4b5e9804318fad72c8503c430134db
akhil-2kb2 <rudraaraju@gmail.com>
2025-03-04 15:22:27 +0530

    changes in Header.tsx

--- starter-template/src/sections/Header.tsx
+++ starter-template/src/sections/Header.tsx
@@ -2,14 +2,18 @@ export const Header = () => {
   return (
   <div className="flex justify-center items-center relative top-3">
     <nav>
-      <a href="#"
-      className="nav-item"
-      >
+      <a href="#"className="nav-item">
         Home
       </a>
-      <a href="#">Projects</a>
-      <a href="#">About</a>
-      <a href="#">Contact</a>
+      <a href="#"className="nav-item">
+        Projects
+      </a>
+      <a href="#"className="nav-item">
+        About
+      </a>
+      <a href="#"className="nav-item">
+        Contact
+    </a>
     </nav>
   </div>
   );
