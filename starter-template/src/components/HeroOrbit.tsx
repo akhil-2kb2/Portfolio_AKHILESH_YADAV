56982fe4358985124232a6682fbb390ca4619178
akhil-2kb2 <rudraaraju@gmail.com>
2025-03-05 02:24:32 +0530

    changes in Hero.tsx adding HeroOrbit.tsx in component

--- starter-template/src/components/HeroOrbit.tsx
+++ starter-template/src/components/HeroOrbit.tsx
@@ -7,13 +7,14 @@ export const HeroOrbit = ({
       }: PropsWithChildren <{ size: number; rotation: number; }>) => {
     return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
-     <div className="" 
+     <div className="flex items-start justify-start" 
        style={{
             transform: `rotate(${rotation}deg)`,
             height: `${size}px`,
             width: `${size}px`,
          }}>
-        <div className=" inline-flex" style={{
+        <div className=" inline-flex " 
+        style={{
             transform: `rotate(${rotation * -1}deg)`,
           
         }}> 
