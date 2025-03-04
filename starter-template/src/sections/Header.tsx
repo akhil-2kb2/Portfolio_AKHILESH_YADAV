e2ae8f54d9f4926105331a83533f3b272ccd6797
akhil-2kb2 <rudraaraju@gmail.com>
2025-03-04 14:54:01 +0530

    changes in globals.css

--- starter-template/src/sections/Header.tsx
+++ starter-template/src/sections/Header.tsx
@@ -2,7 +2,11 @@ export const Header = () => {
   return (
   <div className="flex justify-center items-center relative top-3">
     <nav>
-      <a href="#">Home</a>
+      <a href="#"
+      className="nav-item"
+      >
+        Home
+      </a>
       <a href="#">Projects</a>
       <a href="#">About</a>
       <a href="#">Contact</a>
