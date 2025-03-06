8cf3396f25a795d9d42d2b5ef8db9855d38c5a9b
akhil-2kb2 <rudraaraju@gmail.com>
2025-03-06 11:29:26 +0530

    updating   Tape.tsx & Page.tsx

--- starter-template/src/sections/Tape.tsx
+++ starter-template/src/sections/Tape.tsx
@@ -1,3 +1,28 @@
+import StarIcon from "@/assets/icons/star.svg";
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User-friendly",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+        <div>
+          <div className="flex gap-4">
+          {words.map(word => (
+            <div key={word}>
+              <span>{word}</span>
+              <StarIcon />
+            </div>
+          ))}
+          </div>
+        </div>
+  );
 };
