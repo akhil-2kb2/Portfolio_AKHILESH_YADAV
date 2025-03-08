84f8fc72e17a99cb2b2dd5ff2b0fcef267267108
akhil-2kb2 <rudraaraju@gmail.com>
2025-03-08 18:54:18 +0530

    ...

--- starter-template/src/sections/About.tsx
+++ starter-template/src/sections/About.tsx
@@ -98,28 +98,30 @@ export const AboutSection = () => {
         title="A Glimpse Into My World" 
         description="Learn More About Me Who I Am, What I Do, And What Inspires Me"
         />
-        <div className="mt-20 flex flex-col gap-6">
-          <Card className="h-[320px]">
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5">
+          <Card className="h-[320px] md:col-span-2">
             <CardHeader title="My Reads" description="Explore The Books Shaping My Perspectives." />
-             <div className="w-40 mx-auto mt-8">
+             <div className="w-40 mx-auto mt-2">
                <Image src ={bookImage} alt="Book Cover" 
             />
              </div>
           </Card>
-          <Card className="h-[320px] p-0">
-          <CardHeader 
-          title="My Toolbox" 
-          description="Explore The Technologies And Tools I Use To Craft Exceptional Digital Experiences." 
-          className="px-6 pt-6"
-          />
-            <ToolboxItems items={toolboxItems} className="mt-6" />
-            <ToolboxItems 
-            items={toolboxItems} 
-            className="mt-6" 
-            itemsWrapperClassName="-translate-x-1/2"
-            />
+          <Card className="h-[320px] md:col-span-3">
+              <CardHeader 
+              title="My Toolbox" 
+              description="Explore The Technologies And Tools I Use To Craft Exceptional Digital Experiences." 
+              className=""
+              />
+                <ToolboxItems items={toolboxItems} className="" />
+                <ToolboxItems 
+                items={toolboxItems} 
+                className="mt-6" 
+                itemsWrapperClassName="-translate-x-1/2"
+                />
             
-          </Card>
+           </Card>
+          </div>
           <Card className="h-[320px] p-0 flex flex-col">
           <CardHeader 
             title="Beyond The Code" 
